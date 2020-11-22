25c33b51768c2caa7bdbdfa555211e2efbbedd6c
you06 <you1474600@gmail.com>
2020-11-23 01:30:33 +0800

    revert handle for out of range
    Signed-off-by: you06 <you1474600@gmail.com>

--- lindera-core/src/core/connection.rs
+++ lindera-core/src/core/connection.rs
@@ -17,12 +17,6 @@ impl ConnectionCostMatrix {
 
     pub fn cost(&self, forward_id: u32, backward_id: u32) -> i32 {
         let cost_id = (backward_id + forward_id * self.backward_size) as usize;
-        match self.costs_data.get(cost_id * 2) {
-            Some(v) => unsafe {
-                LittleEndian::read_i16(&[*v, *self.costs_data.get_unchecked(cost_id * 2 + 1)])
-                    as i32
-            },
-            None => 0,
-        }
+        LittleEndian::read_i16(&self.costs_data[cost_id * 2..]) as i32
     }
 }
